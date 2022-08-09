fa1956b9c0f6e84be3caf26864cfa41c41c568d0
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-08-09 23:18:39 +0300

    Add mouse2steer.py

---
+++ mouse2steer.py
@@ -0,0 +1,35 @@
+import evdev
+import joystick
+
+STEERING_MIN, STEERING_MAX = (-32767, 32767)
+PHYSICAL_STEER_MIN, PHYSICAL_STEER_MAX = (-8191, 8191)
+
+v = joystick.VirtualJoystick("Mouse to Steering Wheel Controller")
+
+def get_device():
+    for p in evdev.list_devices():
+        dv = evdev.InputDevice(p)
+        cap = dv.capabilities()
+        if evdev.ecodes.EV_REL in cap:
+            yield dv
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+try:
+    mouse = next(get_device())
+except StopIteration:
+    print("Mouse not found, make sure you have access to /dev/input/eventX")
+    exit()
+
+print("Found mouse: ", mouse.name)
+
+try:
+    x = 0
+    for event in mouse.read_loop():
+        if event.code == 0:
+            x += event.value
+            v.emit(map_range(x, PHYSICAL_STEER_MIN, PHYSICAL_STEER_MAX, STEERING_MIN, STEERING_MAX))
+
+except KeyboardInterrupt:
+    del v
